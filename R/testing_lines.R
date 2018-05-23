adf85a085913b1ebe740f763469a0116f93b58da
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-05-23 12:09:48 -0400

    update test lines

--- R/testing_lines.R
+++ R/testing_lines.R
@@ -1,9 +1,15 @@
 #Add in multiple segements to see how this works.
-x <- c(2,2,2,2,2)
-y <- c(5,4,3,2,1)
-simple_line <- st_linestring(as.matrix(data.frame(x,y)))
+library(sf)
+library(dplyr)
+l1 <- matrix(c(3,5,3,4), ncol = 2, byrow = TRUE)
+l2 <- matrix(c(3,4,3,2), ncol = 2, byrow = TRUE)
+l3 <- matrix(c(3,2,3,0), ncol = 2, byrow = TRUE)
+ml1 <- list(l1,l2,l3)
+simple_line <- st_multilinestring(ml1)
 simple_line_sfc <- st_sfc(simple_line)
-simple_line_sf <- st_sf(id = "A", geometery = simple_line_sfc)
+simple_line_sf <- st_sf(geometery = simple_line_sfc) %>%
+  mutate(cols = c("red","green","blue"))
+
 x2 <- c(0,1,2,2,3)
 y2 <- c(5,4,3,2,1)
 simple_line2 <- st_linestring(as.matrix(data.frame(x2,y2)))
