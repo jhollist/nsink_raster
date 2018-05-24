836accd733b529eeaa630dab9fd22ff8d0290e47
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-05-23 21:12:19 -0400

    graph shortest path working!

--- graph_work.R
+++ graph_work.R
@@ -1,14 +1,13 @@
 library(igraph)
 gdf <- data.frame(from = c(1,2,3,5), to = c(2,3,4,3), id = c("A","B","C","D"))
-g1 <- graph_from_data_frame(gdf, directed = TRUE)
+g1 <- graph_from_data_frame(gdf, directed = FALSE)
 plot(g1)
-#Shortest Path with the fromnode at the top and fromnode at the bottom
-#edge_attr will return labels (use stream_comid)
-#not working completely yet.
-idx <- all_simple_paths(g1, 5, 4, "all")[[1]]
-igraph::edge_attr(g1, "id")[idx]
+idx <- shortest_paths(g1, "1", "4", output = "epath", mode = "out")$epath[[1]]
+igraph::edge_attr(g1, "id", idx)
 
 
-#g1_vec <- c(1,2,2,3,3,4,4,5,5,6,7,8,8,3,9,10,10,11,11,8,12,13,13,5,14,13)
-#g1_df <- data.frame(matrix(g1_vec, ncol = 2, byrow = T), id = LETTERS[1:13])
-#g1 <- graph_from_data_frame(g1_df, directed = TRUE)
+
+g2_vec <- c(1,2,2,3,3,4,4,5,5,6,7,8,8,3,9,10,10,11,11,8,12,13,13,5,14,13)
+g2_df <- data.frame(matrix(g2_vec, ncol = 2, byrow = T), id = LETTERS[1:13])
+g2 <- graph_from_data_frame(g2_df, directed = TRUE)
+plot(g2)
